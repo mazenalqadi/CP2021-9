ce16e0fc2c988af5075ab7bd6c6f18556b1836bf
mazenalqadi <83319181+mazenalqadi@users.noreply.github.com>
2021-04-29 04:46:59 +0200

    Update Wieringermeer-landfill.py

--- Wieringermeer-landfill.py
+++ Wieringermeer-landfill.py
@@ -28,17 +28,43 @@ MetoData = pd.read_excel ('C:/Users/mazen/Desktop/MetoData.xlsx', index_col = 0)
 
 Qdr = LeachateData.iloc[:, 1]                   # leachate output in [m^3/day]
 Jrf = MetoData.iloc[-len(Qdr) + 1 : -1, 1]      # Precipitation [m/day]
-E = MetoData.iloc[-(len(Qdr) +1): -1, 2]        # Evaporation  [m/day]
+pE = MetoData.iloc[-(len(Qdr) +1): -1, 2]        # Evaporation  [m/day]
 
 # Definition of rate equation 
 def dydt(t,S):
   """Return the rate of change of storages"""
   Scl = S[0]
   Swb = S[1]
+  
   Seff_cl = (Scl - Sclmin)/(Sclmax - Sclmin)
   Lcl = acl * Seff_cl**bcl
   
-  #Equations
+  Seff_wb = (Swb - Swbmin)/(Swbmax - Swbmin)
+  Lwb = awb * Seff_wb**bwb
+  
+  E = pE * Cf *fred
+  Beta = Beta0 * Seff_cl
+  
+  # Equations
   dScldt = Jrf - Lcl - E
+  dSwbdt = (1 - Beta) * Lcl - Lwb
 
   return np.array([dScldt, dSwbdt,Qdr])
+
+# Definition of parameters
+
+Sclmax = 0.3 * 15   # Maximum storage is reached when all porosity is filled with water [m]
+Sclmin = 0          # Minimum storage range between zero and 5 [m], Zero is chosen as the initial value 
+bcl = 1             # Assumed
+acl = #(10% * max(Qdr)value + max (Qdr value) [in m/day] - here we should make use of the 
+
+Swbmax = 7.5        # how 
+Swbmin = 0          #
+awb = 0.001         # how
+bwb = 30            #
+
+Beta0 = 0.92        #
+Cf = 0.92           #
+fred = 1            # Assuming no reduction in evaporation 
+
+# Initial case:
